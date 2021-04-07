78b3de027a3b3c81d40b3cf586a68dc04bffa52f
Carlos Henrique <chsjr1996@gmail.com>
2021-04-07 13:06:38 -0300

    Improve search usability

--- js/toggle-search.js
+++ js/toggle-search.js
@@ -1,13 +1,16 @@
 ( function( $ ) {
     $( '.search-submit' ).on( 'click', function() {
-        $( '.search-field' ).toggleClass( 'toggle-search' );
-
-        if ( $( '.search-field' ).hasClass( 'toggle-search' ) ) {
+        if ( !( $( '.search-field' ).hasClass( 'toggle-search' ) ) ) {
+            $( '.search-field' ).addClass( 'toggle-search' );
             $( '.search-field' ).focus();
+        } else if ( $( '.desktop-search-container .search-field' ).val() ) {
+            $( '.desktop-search-container .search-form' ).submit();
         }
     } );
 
     $( '.search-field' ).on( 'blur', function() {
-        $( '.search-field' ).removeClass( 'toggle-search' );
+        setTimeout( function() {
+            $( '.search-field' ).removeClass( 'toggle-search' );
+        }, 200 )
     } );
 } )( jQuery )
