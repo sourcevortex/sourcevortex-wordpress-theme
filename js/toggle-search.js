175ca6321019e0f6608524f4b3dc58b3aa90231a
Carlos Henrique <chsjr1996@gmail.com>
2021-04-07 11:53:47 -0300

    Fix search input

--- js/toggle-search.js
+++ js/toggle-search.js
@@ -1,5 +1,13 @@
 ( function( $ ) {
     $( '.search-submit' ).on( 'click', function() {
         $( '.search-field' ).toggleClass( 'toggle-search' );
-    })
+
+        if ( $( '.search-field' ).hasClass( 'toggle-search' ) ) {
+            $( '.search-field' ).focus();
+        }
+    } );
+
+    $( '.search-field' ).on( 'blur', function() {
+        $( '.search-field' ).removeClass( 'toggle-search' );
+    } );
 } )( jQuery )
